eb3dfaf52fc072cd0bbb5cd4902809ab0d2e7cca
Sebastian Subola <sebastian.subola@gmail.com>
2022-09-25 16:02:34 -0600

    Add mouseover listener to etchableDiv class
    The mouseover listener turns the event target (e.target) black upon
    mouse-over, in this case the etchable element. Add the 'once' option to
    prevent unecessary listening once turned black.
    
    Note: Had a lot of confusion trying to modify the etchableDiv element
    using 'this' instead of 'e.target'. Need to become more familliar with
    that.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* Initial DOM Manipulation */
 
 initalizeDrawspace();
-
+initializeMouseOverListener();
 
 // Generate a width * width square grid of div elements within the drawspace div
 function initalizeDrawspace(edge = 16) {
@@ -9,9 +9,25 @@ function initalizeDrawspace(edge = 16) {
 
     for(let i = 0; i < edge ** 2; i++) {
         etchableDiv = document.createElement('div');
+        etchableDiv.classList.add('etchable');
         etchableDiv.style['width'] = (100 / edge) + '%';
         etchableDiv.style['aspect-ratio'] = '1 / 1';
         drawspace.appendChild(etchableDiv);
     }
 
+}
+
+function initializeMouseOverListener() {
+    etchables = document.querySelectorAll('.etchable');
+
+    etchables.forEach(etchable => 
+        etchable.addEventListener('mouseover',
+        e => {
+            console.log(e.target);
+            e.target.style['background-color'] = 'black';
+        },
+        {
+            once: true // Only need to activate once to color in full black, but will need to be removed to do the shading
+        })
+    );
 }
