29d7572a9e0f0c0e957f776f5008f73024c6b7cd
Sebastian Subola <sebastian.subola@gmail.com>
2022-09-25 15:23:15 -0600

    Add styling to etchableDiv within drawspace
    The etchableDiv 'pixels' are now automatically sized via
    initializeDrawspace() in script.js to be square and use box-sizing:
    border-box to ensure the calculated width is correct regardless of
    padding or content. The aspect-ratio style also keeps the 'pixel'
    square.

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ initalizeDrawspace();
 
 
 // Generate a width * width square grid of div elements within the drawspace div
-function initalizeDrawspace(width = 16) {
+function initalizeDrawspace(edge = 16) {
     drawspace = document.querySelector('.drawspace');
-    
-    for(let i = 0; i < width ** 2; i++) {
-        drawspace.appendChild(document.createElement('div'));
+
+    for(let i = 0; i < edge ** 2; i++) {
+        etchableDiv = document.createElement('div');
+        etchableDiv.style['width'] = (100 / edge) + '%';
+        etchableDiv.style['aspect-ratio'] = '1 / 1';
+        drawspace.appendChild(etchableDiv);
     }
 
 }
